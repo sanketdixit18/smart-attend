4b76ea5abd90f9d56ff4f4d16477cf598f64a062
sanketdixit18 <sanketdixit424@gmail.com>
2026-04-10 12:03:47 +0530

    fix db connection

--- lib/db.ts
+++ lib/db.ts
@@ -3,6 +3,7 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
+  port: Number(process.env.DB_PORT),
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'smart_attend',
   waitForConnections: true,
